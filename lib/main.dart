61127a6a5ac8c0f072703ad0de90d9c58b6dd32e
Navya Garg <navgarg06@gmail.com>
2022-01-13 17:04:07 +0530

    fix: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Exun App 2021-22',
       theme: ThemeData(
         fontFamily: "Trebuchet MS",
